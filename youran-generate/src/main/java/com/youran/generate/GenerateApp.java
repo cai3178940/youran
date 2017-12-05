e7668ac5a2d94ad8a600712157003e0c7ffb0927
蔡斌冰 <caibinbing@foxmail.com>
2017-12-05 16:56:08 +0800

    兼容tomcat部署模式

--- youran-generate/src/main/java/com/youran/generate/GenerateApp.java
+++ youran-generate/src/main/java/com/youran/generate/GenerateApp.java
@@ -4,6 +4,8 @@ import com.didispace.swagger.EnableSwagger2Doc;
 import com.youran.common.optimistic.EnableOptimisticLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Title:
@@ -14,11 +16,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableSwagger2Doc
 @EnableOptimisticLock
-public class GenerateApp {
+public class GenerateApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(GenerateApp.class);
         app.run(args);
+    }
+
+    /**
+     * 兼容tomcat部署模式
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GenerateApp.class);
     }
 
 }
--- youran-generate/src/main/resources/ftl/sources/{packageName}/{ProjectName}App.java.ftl
+++ youran-generate/src/main/resources/ftl/sources/{packageName}/{ProjectName}App.java.ftl
@@ -5,16 +5,27 @@ import com.didispace.swagger.EnableSwagger2Doc;
 import ${commonPackage}.optimistic.EnableOptimisticLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 <@classCom "启动类"></@classCom>
 @SpringBootApplication
 @EnableSwagger2Doc
 @EnableOptimisticLock
-public class ${ProjectName}App {
+public class ${ProjectName}App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(${ProjectName}App.class);
         app.run(args);
     }
 
+    /**
+     * 兼容tomcat部署模式
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GenerateApp.class);
+    }
 }
