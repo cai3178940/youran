e669399bb8592cb2b6b2f2afc04bd40b0cb2a7d4
蔡斌冰 <caibinbing@foxmail.com>
2017-12-05 16:43:09 +0800

    配置入口地址

--- youran-generate/src/main/java/com/youran/generate/config/GenerateMvcConfig.java
+++ youran-generate/src/main/java/com/youran/generate/config/GenerateMvcConfig.java
@@ -36,6 +36,8 @@ public class GenerateMvcConfig extends WebMvcConfigurerAdapter {
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController( "" ).setViewName( "forward:/ui/index.html" );
+        registry.addViewController( "/" ).setViewName( "forward:/ui/index.html" );
         registry.addViewController( "/ui" ).setViewName( "forward:/ui/index.html" );
         registry.addViewController( "/ui/" ).setViewName( "forward:/ui/index.html" );
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
