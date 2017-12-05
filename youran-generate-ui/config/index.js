97f35815ddf68c571952dee693c29628df5ae76b
蔡斌冰 <caibinbing@foxmail.com>
2017-12-05 15:33:49 +0800

    增加前端编译输出配置

--- youran-generate-ui/config/index.js
+++ youran-generate-ui/config/index.js
@@ -50,12 +50,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../youran-generate/src/main/resources/ui/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../youran-generate/src/main/resources/ui'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/ui',
 
     /**
      * Source Maps
--- youran-generate/src/main/java/com/youran/generate/config/GenerateMvcConfig.java
+++ youran-generate/src/main/java/com/youran/generate/config/GenerateMvcConfig.java
@@ -2,6 +2,7 @@ package com.youran.generate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -9,10 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class GenerateMvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
                 .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false).maxAge(3600);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/ui/** 访问都映射到classpath:/ui/ 目录下
+        registry.addResourceHandler("/ui/**").addResourceLocations("classpath:/ui/");
+    }
+
 }
