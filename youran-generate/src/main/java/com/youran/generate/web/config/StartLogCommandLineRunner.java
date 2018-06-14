e1be42ceceebaba752a82bc9a4d5e962550bafc4
蔡斌冰 <caibinbing@foxmail.com>
2018-06-14 11:10:28 +0800

    微调startLog

--- youran-generate/src/main/java/com/youran/generate/web/config/StartLogCommandLineRunner.java
+++ youran-generate/src/main/java/com/youran/generate/web/config/StartLogCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.youran.generate.web.config;
 
 import com.youran.generate.util.IpUtil;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,9 @@ import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Title: 打印启动日志
@@ -31,9 +35,15 @@ public class StartLogCommandLineRunner implements CommandLineRunner,Ordered {
         String port = env.getProperty("server.port","8080");
         String contextPath = env.getProperty("server.context-path","/");
         String applicationName = env.getProperty("spring.application.name","");
+        String profiles = "";
+        if(ArrayUtils.isNotEmpty(env.getActiveProfiles())) {
+            profiles = Arrays.stream(env.getActiveProfiles()).collect(Collectors.joining(","));
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("\n----------------------------------------------------------\n")
-            .append("\t应用【").append(applicationName).append("】已经启动！访问路径：\n")
+            .append("\t应用【").append(applicationName).append("】已经启动！\n")
+            .append("\t激活profile:\t").append(profiles).append("\n")
+            .append("\t访问路径:\n")
             .append("\t本地: \thttp://localhost:").append(port).append(contextPath).append("\n")
             .append("\t外部: \thttp://").append(IpUtil.getLocalIp()).append(":").append(port).append(contextPath).append("\n");
         ClassPathResource swaggerUIResource = new ClassPathResource("META-INF/resources/swagger-ui.html");
@@ -49,4 +59,5 @@ public class StartLogCommandLineRunner implements CommandLineRunner,Ordered {
     public int getOrder() {
         return Ordered.LOWEST_PRECEDENCE;
     }
+
 }
--- youran-generate/src/main/resources/ftl/root/{webModule}/src/main/java/{packageName}/web/config/StartLogCommandLineRunner.java.ftl
+++ youran-generate/src/main/resources/ftl/root/{webModule}/src/main/java/{packageName}/web/config/StartLogCommandLineRunner.java.ftl
@@ -3,6 +3,7 @@
 <#--定义主体代码-->
 <#assign code>
 <@import "${packageName}.web.util.IpUtil"/>
+<@import "org.apache.commons.lang3.ArrayUtils"/>
 <@import "org.slf4j.Logger"/>
 <@import "org.slf4j.LoggerFactory"/>
 <@import "org.springframework.boot.CommandLineRunner"/>
@@ -10,6 +11,9 @@
 <@import "org.springframework.core.env.Environment"/>
 <@import "org.springframework.core.io.ClassPathResource"/>
 <@import "org.springframework.stereotype.Component"/>
+
+<@import "java.util.Arrays"/>
+<@import "java.util.stream.Collectors"/>
 <@classCom "打印启动日志"/>
 @Component
 public class StartLogCommandLineRunner implements CommandLineRunner,Ordered {
@@ -27,9 +31,15 @@ public class StartLogCommandLineRunner implements CommandLineRunner,Ordered {
         String port = env.getProperty("server.port","8080");
         String contextPath = env.getProperty("server.context-path","/");
         String applicationName = env.getProperty("spring.application.name","");
+        String profiles = "";
+        if(ArrayUtils.isNotEmpty(env.getActiveProfiles())) {
+            profiles = Arrays.stream(env.getActiveProfiles()).collect(Collectors.joining(","));
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("\n----------------------------------------------------------\n")
-            .append("\t应用【").append(applicationName).append("】已经启动！访问路径：\n")
+            .append("\t应用【").append(applicationName).append("】已经启动！\n")
+            .append("\t激活profile:\t").append(profiles).append("\n")
+            .append("\t访问路径:\n")
             .append("\t本地: \thttp://localhost:").append(port).append(contextPath).append("\n")
             .append("\t外部: \thttp://").append(IpUtil.getLocalIp()).append(":").append(port).append(contextPath).append("\n");
         ClassPathResource swaggerUIResource = new ClassPathResource("META-INF/resources/swagger-ui.html");
