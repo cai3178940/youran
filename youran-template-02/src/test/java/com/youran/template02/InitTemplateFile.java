39664f35786a50e6ec41df5bd539c7704334b70f
caibinbing <caibinbing@foxmail.com>
2019-11-21 22:27:50 +0800

    fix: 准备开发vue前端模板

--- youran-template-02/src/test/java/com/youran/template02/InitTemplateFile.java
+++ youran-template-02/src/test/java/com/youran/template02/InitTemplateFile.java
@@ -1,12 +1,16 @@
 package com.youran.template02;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.HiddenFileFilter;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.Git;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.function.Consumer;
 
@@ -20,12 +24,29 @@ public class InitTemplateFile {
 
     public static final String REMOTE_URL = "https://github.com/PanJiaChen/vue-admin-template.git";
     public static final String BRANCH_TO_CLONE = "master";
+    public static final String FTL_DIR = "D:/workspace_cbb/youran/youran-template-02/src/main/resources/ftl";
+    public static final String[] ASSETS_EXTS = {
+        "ico",
+        "png",
+        "svg"
+    };
+    public static final String[] NOT_CONVERT_FTL_FILE = {
+        "LICENSE"
+    };
+    public static final String[] IGNORE_FILE_PATH_PREFIX = {
+        "/src/views/nested"
+    };
+
 
     @Test
     public void initCode() throws Exception {
+        // 先从github下载vue-admin-template原始代码
         //File repoDir = this.checkOutVueAdminTemplate();
-        File repoDir = new File("C:/Users");
-        this.onEachRepoFile(repoDir,this::copyRepoFile);
+        File repoDir = new File("C:\\Users\\caibi\\AppData\\Local\\Temp\\vue-admin-template8463170824386821771");
+        // 清空模板目录
+        this.cleanFtlDir();
+        // 遍历原始代码，并拷贝模板文件
+        this.onEachRepoFile(repoDir, repoFile -> this.copyRepoFile(repoDir, repoFile));
     }
 
     /**
@@ -47,7 +68,18 @@ public class InitTemplateFile {
     }
 
     /**
-     * 遍历每个原始文件
+     * 清空模板目录
+     *
+     * @throws Exception
+     */
+    @Test
+    public void cleanFtlDir() throws Exception {
+        File ftlDir = new File(FTL_DIR);
+        FileUtils.cleanDirectory(ftlDir);
+    }
+
+    /**
+     * 遍历每个源码文件
      *
      * @param dir
      */
@@ -78,12 +110,60 @@ public class InitTemplateFile {
     }
 
     /**
-     * 将原始代码拷贝成freemarker模板文件
+     * 将源码文件拷贝成freemarker模板文件
      *
+     * @param repoDir
      * @param repoFile
      */
-    private void copyRepoFile(File repoFile) {
-        System.out.println(repoFile.getPath());
+    private void copyRepoFile(File repoDir, File repoFile) {
+        String path = repoFile.getPath().substring(repoDir.getPath().length())
+            .replaceAll("\\\\", "/");
+        System.out.println(path);
+        if (this.isRepoFileIgnore(path)) {
+            return;
+        }
+        String target = FTL_DIR + path;
+        try {
+            if (this.neetConvertFtl(repoFile)) {
+                String content = FileUtils.readFileToString(repoFile, "utf-8");
+                FileUtils.write(new File(target + ".ftl"), content, "utf-8");
+            } else {
+                FileUtils.copyFile(repoFile, new File(target));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * 判断是否需要忽略源码文件
+     *
+     * @param repoFilePath
+     * @return
+     */
+    private boolean isRepoFileIgnore(String repoFilePath) {
+        return Arrays.stream(IGNORE_FILE_PATH_PREFIX)
+            .anyMatch(prefix -> repoFilePath.startsWith(prefix));
+    }
+
+    /**
+     * 判断源码文件是否需要转换成ftl模板文件
+     *
+     * @param repoFile
+     * @return
+     */
+    private boolean neetConvertFtl(File repoFile) {
+        String name = repoFile.getName();
+        if (ArrayUtils.contains(NOT_CONVERT_FTL_FILE, name)) {
+            return false;
+        }
+        String extension = FilenameUtils.getExtension(name);
+        if (ArrayUtils.contains(ASSETS_EXTS, StringUtils.lowerCase(extension))) {
+            return false;
+        }
+        return true;
     }
 
 
