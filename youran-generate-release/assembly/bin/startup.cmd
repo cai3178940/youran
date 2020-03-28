2f82b00694041d98566bef82fd547968d6dbbcd6
caibinbing <caibinbing@foxmail.com>
2020-03-29 03:28:13 +0800

    fix: 修复linux部署失败的bug

--- youran-generate-release/assembly/bin/startup.cmd
+++ youran-generate-release/assembly/bin/startup.cmd
@@ -1,5 +1,5 @@
 @echo off
-set APP_HOME=..
+set APP_HOME=%~dp0\..
 set CLASS_PATH=%APP_HOME%/lib/*;%APP_HOME%/resource;%APP_HOME%/conf;
 set APP_MAIN_CLASS=com.youran.generate.GenerateApp
 java -classpath %CLASS_PATH% %APP_MAIN_CLASS%
--- youran-generate-release/assembly/bin/startup.sh
+++ youran-generate-release/assembly/bin/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-APP_HOME=..
+PRGDIR=`dirname "$PRG"`
+APP_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
 APP_MAIN_CLASS="com.youran.generate.GenerateApp"
 CLASS_PATH="$APP_HOME/conf:$APP_HOME/lib/*:$APP_HOME/resource"
 java -classpath $CLASS_PATH $APP_MAIN_CLASS
