562a433bed3a600d3779b61e0ade081ce1c45fdd
caibinbing <caibinbing@foxmail.com>
2020-03-29 23:03:15 +0800

    fix: 修复linux部署失败的bug

--- youran-generate-release/assembly/bin/startup.sh
+++ youran-generate-release/assembly/bin/startup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
 PRGDIR=`dirname "$PRG"`
 APP_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
 APP_MAIN_CLASS="com.youran.generate.GenerateApp"
